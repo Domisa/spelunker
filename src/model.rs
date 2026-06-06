8caa73db8d50ff31cf60c853142652a04e27d85a
Dom Molsbarger <molsbarger.daniel@gmail.com>
2026-06-06 01:27:23 -0500

    feat: stub load config function in model.rs

--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,5 @@
+use std::error:Error;
+
 #[derive(Deserialize)]
 pub struct ServiceTarget {
     pub service_name: String,
@@ -8,5 +10,10 @@ pub struct ServiceTarget {
 #[derive(Deserialize)]
 pub struct ServiceList {
     pub service_list: Vec<ServiceTarget>,
-    pub num_trails: u32,
+    //pub num_trails: u32,
+}
+
+pub fn load_config (path: &str) -> Result<ServiceList, Box<dyn Error>>{
+    let _data = std::fs::read_to_string(path);
+    todo!();
 }
