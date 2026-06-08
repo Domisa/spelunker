8ffa7353f0d3a917511a11517c7bff54e57a5663
Dom Molsbarger <molsbarger.daniel@gmail.com>
2026-06-07 21:40:09 -0500

    feat: implement load config to parse a TOML file

--- src/model.rs
+++ src/model.rs
@@ -14,6 +14,7 @@ pub struct ServiceList {
 }
 
 pub fn load_config (path: &str) -> Result<ServiceList, Box<dyn Error>>{
-    let _data = std::fs::read_to_string(path);
+    let contents = std::fs::read_to_string(path)?;
+    let config: ServiceList = toml::from_str(&contents)?;
     todo!();
 }
