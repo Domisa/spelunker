5fa0e701cee3014a11c5c4a616fcc8957dffabfa
Dom Molsbarger <molsbarger.daniel@gmail.com>
2026-06-10 22:26:06 -0500

    feat: added args struct for Clap implementation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 use model::load_config;
+use clap::Parser;
 
 mod stats;
 mod model;
 mod simulation;
 
+#[derive(Parser)]
+struct Args {
 
+    #[arg(short, long)]
+    config: String,
+
+    #[arg(short, long)]
+    iterations: u64,
+}
 
 fn main() {
     
