584ab52e4c73a630c93c3aba5d69be5987440eae
Dom Molsbarger <molsbarger.daniel@gmail.com>
2026-06-13 18:16:25 -0500

    feat: added walk_dag to traverse the DAG for each run of the simulation

--- src/simulation.rs
+++ src/simulation.rs
@@ -1,3 +1,24 @@
 // This is where the Monte Carlo calculation happens
 use rayon;
+use crate::model::{TrialResult, ServiceList};
+use std::collections::HashMap;
+
+fn walk_dag(dag: &HashMap<String, Vec<String>>, service: &str, visited: &mut HashMap<String, TrialResult>) -> TrialResult {
+    
+    if let Some(cached) = visited.get(service) {
+        return cached.clone();
+    }
+    
+    if let Some(dependency) = dag.get(service) {
+        for dep in dependency {
+             let dep_result = walk_dag(dag, dep, visited);
+             if dep_result.failure {
+                return TrialResult {failure: true, latency: 0.0};
+             }
+        }
+       
+    }
+
+    todo!();
+}
 
