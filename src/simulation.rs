b8d311188a3c6af159183878e30f1bbe482ceaa4
Dom Molsbarger <molsbarger.daniel@gmail.com>
2026-06-16 17:06:40 -0500

    feat: added sample_latency function to simulate a possible latency for a run.

--- src/simulation.rs
+++ src/simulation.rs
@@ -2,6 +2,7 @@
 use rayon;
 use crate::model::{TrialResult, ServiceList};
 use std::collections::HashMap;
+use rand_distr::{LogNormal, Distribution};
 
 fn walk_dag(dag: &HashMap<String, Vec<String>>, service: &str, visited: &mut HashMap<String, TrialResult>) -> TrialResult {
     
@@ -28,3 +29,11 @@ fn sample_failure(failure_rate: f64) -> bool {
     n < failure_rate
 }
 
+fn sample_latency(median_latency: f64, std_dev: f64) -> f64 {
+
+    //replace unwrap for better error handling later
+    let mu = median_latency.ln();
+    let dist = LogNormal::new(mu, std_dev).unwrap();
+    let latency = dist.sample(&mut rand::rng());
+    latency
+}
