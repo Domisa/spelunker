c6235ed0fdc5c56c6cc2b29e384e074e30a20f93
Domisa <molsbarger.daniel@gmail.com>
2026-08-19 23:43:52 -0500

    feat: added failure_rate calculation for the results

---
+++ src/stats.rs
@@ -0,0 +1,20 @@
+use crate::model::TrialResult;
+
+pub struct Data {
+    pub failure_rate: f64,
+    pub latency: f64,
+    pub availability: f64,
+    pub p50: f64,
+    pub p95: f64,
+    pub p99: f64,
+}
+
+fn failure_rate(results: &Vec<TrialResult>) -> f64 {
+    let mut fail_count: u32 = 0;
+    for result in results {
+        if result.failure {
+        fail_count += 1;
+        }
+    }
+    fail_count as f64/results.len() as f64
+}
