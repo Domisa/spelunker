f2e1ffab09909b9b17dbab34d00a4f444d583f33
Domisa <molsbarger.daniel@gmail.com>
2026-09-05 23:55:13 -0500

    feat: implemented model, simulation, and stats in main for v1, now complete.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ mod simulation;
 
 use model::load_config;
 use clap::Parser;
-
+use model::{build_dag, build_service_lookup};
+use simulation::parallel_num_trials;
+use stats::{data_form, to_json};
 
 #[derive(Parser)]
 struct Args {
@@ -13,10 +15,22 @@ struct Args {
     config: String,
 
     #[arg(short, long)]
-    iterations: u64,
+    iterations: u32,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    println!("Hello, world!");
+    let services = load_config(&args.config)?;
+
+    let dag = build_dag(&services);
+    let service_lookup = build_service_lookup(&services);
+
+    let num_trails = parallel_num_trials(&dag, &services, args.iterations);
+    
+    let data = data_form(&num_trails);
+
+    let jsoned_data = to_json(&data);
+
+    println!("{}", jsoned_data?);
+    Ok(())
 }
